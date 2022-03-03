773a41bfaa3babdbd9cf06cea460ffe9c55807e7
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2022-03-03 10:03:24 -0400

    Commit 8

--- module.c
+++ module.c
@@ -4,6 +4,14 @@
 int var3 = 110;
 #endif
 
+#if defined(Variability3) || defined(Variability2) 
+int var32 = 32;
+#endif
+
+#if defined(Variability3) && defined(Variability2) 
+int var3and2 = 32;
+#endif
+
 #ifdef Variability3
 int AnotherMain(){
     var3 = 256;
